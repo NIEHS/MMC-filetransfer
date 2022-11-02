588cf760d046d747a07d187ee6630b32a4ffcf38
Jonathan Bouvette <jonathan.bouvette@gmail.com>
2022-11-02 16:27:18 -0400

    added group and project forms

---
+++ frontend/mmc-vue/src/components/SetupRun.vue
@@ -0,0 +1,47 @@
+<template>
+<form>
+    <div class="form-group">
+        <label>Source Directory</label>    <input class="form-control" id="setupSourceDir" v-model="session.sourceDir">
+        
+    </div>
+
+</form>
+
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+    name: 'SetupRun',
+    setup() {
+        const session = reactive({
+            sourceDir: "",
+            group: "",
+            project: "",
+            sample: "",
+        
+            scope: "",
+            magnification: 0,
+            pixelSize: 0,
+            totalDose: 0,
+            frameNumber: 0,
+            detectorCounts: 0,
+
+            mode: 'spr',
+            tiltAngleOrScheme: '0',
+            filesPattern: null,
+            gainReference: null,
+            date: 'YYYYMMDD',
+
+            status: 'created',
+            startTime: null,
+            endTime: null,
+            durationInHours: null,
+            numberOfMovies: null,
+        })
+        return {session}
+    }
+}
+
+</script>
